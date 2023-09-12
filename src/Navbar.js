b06c00f5c2bac170c95f7aba85579440286abe6e
omkarv94 <105559826+omkarv94@users.noreply.github.com>
2023-09-13 00:29:45 +0530

    FIrst commit
    First-page

---
+++ src/Navbar.js
@@ -0,0 +1,83 @@
+import React, { useState } from "react";
+import "./Navbar.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faVolumeUp, faVolumeMute } from "@fortawesome/free-solid-svg-icons";
+import Dropdown from 'react-dropdown';
+import 'react-dropdown/style.css';
+
+function Navbar() {
+  const options = ['English', 'Hindi'];
+
+  const [isSoundOn, setIsSoundOn] = useState(true);
+
+  const toggleSound = () => {
+    setIsSoundOn((prevIsSoundOn) => !prevIsSoundOn);
+    // You can add logic here to mute/unmute sound as needed
+  };
+
+  const [active, setActive] = useState("nav__menu");
+  const [icon, setIcon] = useState("nav__toggler");
+  const navToggle = () => {
+    if (active === "nav__menu") {
+      setActive("nav__menu nav__active");
+    } else setActive("nav__menu");
+
+    // Icon Toggler
+    if (icon === "nav__toggler") {
+      setIcon("nav__toggler toggle");
+    } else setIcon("nav__toggler");
+  };
+
+  const defaultOption = options[0];
+
+  return (
+    <nav className="nav">
+      <div className="Time">
+        TIME
+      </div>
+      <a href="#" className="nav__brand">
+        Skip to main content
+      </a>
+      <div onClick={toggleSound} className="nav__toggler">
+        {isSoundOn ? (
+          <FontAwesomeIcon icon={faVolumeUp} className="sound-icon" />
+        ) : (
+          <FontAwesomeIcon icon={faVolumeMute} className="sound-icon" />
+        )}
+      </div>      
+      <ul className={active}>
+        <li className="nav__item">
+          <a href="#" className="nav__link">
+            A+
+          </a>
+        </li>
+        <li className="nav__item">
+          <a href="#" className="nav__link">
+            A
+          </a>
+        </li>
+        <li className="nav__item">
+          <a href="#" className="nav__link">
+            A-
+          </a>
+        </li>
+        <li className="nav__item">
+          <button className="btn1">A</button>
+        </li>
+        <li className="nav__item">
+          <button className="btn2">A</button>
+        </li>
+        <li>
+        <Dropdown options={options} onChange={(selectedOption) => console.log(selectedOption)} value={defaultOption} placeholder="Select an option" />
+        </li>
+      </ul>
+      <div onClick={navToggle} className={icon}>
+        <div className="line1"></div>
+        <div className="line2"></div>
+        <div className="line3"></div>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
